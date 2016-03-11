47579ce26d64fd10c462f52cd23c1e2ffa849be6
gaoyang <447984536@qq.com>
2016-03-11 10:49:17 +0800

    项目初始化

---
+++ synchapter-web/src/main/java/com/ebupt/homework/synchapter_web/App.java
@@ -0,0 +1,13 @@
+package com.ebupt.homework.synchapter_web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
